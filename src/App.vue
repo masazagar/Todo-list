86cfdf78de9861d0abc6d511e93aad3349a43c2d
Masa Zagar <masa.zagar@telekom.si>
2024-03-25 13:45:37 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <section class="main-all">
+    <header>
+      <h1>TO DO LIST</h1>
+    </header>
+    <todo-list></todo-list>
+  </section>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script></script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+header {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  font-family: sans-serif;
+  display: flex;
+  justify-content: center;
+  background-color: rgb(255, 249, 241);
+  color: rgb(255, 249, 241);
+}
+button {
+  font-family: inherit;
+  background-color: black;
+  color: white;
+  border: none;
+  padding: 7px;
+  margin-left: 5px;
+  border-radius: 5px;
+}
+
+input {
+  background-color: rgb(224, 191, 224);
+  border: none;
+  padding: 7px;
+  border-radius: 5px;
+}
+
+.main-all {
+  background-color: darkmagenta;
+  padding: 40px;
+  padding-top: 10px;
+  margin-top: 200px;
+  border-radius: 5px;
+  border: 4px solid black;
+}
+
+ul {
+  list-style: none;
+  width: 100%;
+  margin-right: 40px;
+  padding: 0;
+  text-align: center;
+}
+
+.main-todo {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <main class="main-todo">
+    <form @submit.prevent="addTodo">
+      <input type="text" v-model="input" />
+      <button type="submit">Add todo</button>
+    </form>
+
+    <section class="todo-list">
+      <ul v-for="(todo, index) in todos" :key="index">
+        <li>{{ todo }} <button @click="deleteTodo(index)">X</button></li>
+      </ul>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      input: "",
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.input);
+      this.input = "";
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    },
+  },
+};
+</script>
+
+<style>
+/* body {
+  font-family: sans-serif;
+  display: flex;
+  justify-content: center;
+}
+
+button {
+  font-family: inherit;
+} */
+</style>
